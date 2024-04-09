16644b27206f2af059b67add2c322be659579e8f
martajua <martajua38@gmail.com>
2024-04-09 11:19:22 -0300

    Se realizo el ejercicio 3 del tp1

---
+++ tp1_JuarezMariaMartaDelMilagro/src/ar/edu/unju/fi/ejercicio3/Ejercicio3.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		Scanner leer = new Scanner(System.in);
+
+		System.out.print("Ingrese un número: ");
+		num = leer.nextInt();
+
+		if (num % 2 == 0) {
+			num = num * 3;
+			System.out.println("El triple del numero par es: " + num);
+		} else {
+			num = num * 2;
+			System.out.println("El doble del numero impar es: " + num);
+		}
+		leer.close();
+	}
+
+}
