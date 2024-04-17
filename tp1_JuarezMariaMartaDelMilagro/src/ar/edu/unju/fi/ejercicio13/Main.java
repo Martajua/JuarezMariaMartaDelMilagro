3a18640848ad02c3cbbdc8f6ebd1946cc70507f7
martajua <martajua38@gmail.com>
2024-04-16 22:44:57 -0300

    Se realizo ejercicio 13 del tp1-continuacion

---
+++ tp1_JuarezMariaMartaDelMilagro/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio13;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leer = new Scanner(System.in);
+
+		// declaracion de array
+		int[] numerosEnteros = new int[8];
+
+		// length tamaño del arreglo
+		// carga de elementos al array
+		for (int i = 0; i < numerosEnteros.length; i++) {
+			System.out.print("Ingrese elemento " + (i + 1) + ": ");
+			numerosEnteros[i] = leer.nextInt();
+		}
+		System.out.println("");
+		// vizualizacion de elementos del array
+		for (int i = 0; i < numerosEnteros.length; i++) {
+			System.out.println("Posición " + i + ": " + numerosEnteros[i]);
+		}
+		leer.close();
+	}
+}
